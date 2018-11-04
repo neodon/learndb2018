eccf938786fa4cafe6dda82516b4a90dbb2b98ec
James Barton <james@neodon.com>
2018-11-03 19:40:51 -0500

    ksv-01-simple: add simple implementation

--- src/key-value-store.js
+++ src/key-value-store.js
@@ -1,1 +1,19 @@
-export class KeyValueStore {}
+export class KeyValueStore {
+  constructor() {
+    this.store = {}
+  }
+
+  init() {}
+
+  set(key, value) {
+    this.store[key] = value
+  }
+
+  get(key) {
+    return this.store[key]
+  }
+
+  delete(key) {
+    this.store[key] = undefined
+  }
+}
